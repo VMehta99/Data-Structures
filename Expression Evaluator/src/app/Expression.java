2d6d44d393ecab27040dc65ffaf2e2cee6708947
hariamoor <amoor.hari@gmail.com>
2018-02-21 21:22:46 -0500

    actually did some work this time && git push

--- Expression Evaluator/src/app/Expression.java
+++ Expression Evaluator/src/app/Expression.java
@@ -28,14 +28,14 @@ public class Expression {
             if(delims.contains(Character.toString(expr.toCharArray()[i])))
                 continue;
 
-            else if(i < expr.length() && expr.charAt(i + 1) == '[') {
+            else if(i < (expr.length() - 1) && expr.charAt(i + 1) == '[') {
                 // find last space
                 int j = i;
 
                 while(j > 0 && expr.toCharArray()[j] != ' ')
                     j--;
 
-                if(!arrays.contains(expr.substring(j + 1, i + 1)))
+                if(!arrays.contains(new Array(expr.substring(j + 1, i + 1))))
                     arrays.add(new Array(expr.substring(j + 1, i + 1)));
             }
 
@@ -45,7 +45,7 @@ public class Expression {
                 while(j > 0 && expr.toCharArray()[j] != ' ')
                     j--;
 
-                if(!vars.contains(expr.substring(j + 1, i + 1)))
+                if(!vars.contains(new Variable(expr.substring(j + 1, i + 1))))
                     vars.add(new Variable(expr.substring(j + 1, i + 1)));
             }
         }
@@ -85,23 +85,69 @@ public class Expression {
                     StringTokenizer stt = new StringTokenizer(tok," (,)");
                     int index = Integer.parseInt(stt.nextToken());
                     int val = Integer.parseInt(stt.nextToken());
-                    arr.values[index] = val;              
+                    arr.values[index] = val;
                 }
             }
         }
     }
-    
+
     /**
      * Evaluates the expression.
-     * 
+     *
      * @param vars The variables array list, with values for all variables in the expression
      * @param arrays The arrays array list, with values for all array items
      * @return Result of evaluation
      */
-    public static float 
+    private static int findValue(String name) {
+        for(Variable v : vars)
+            if(v.name.equals(name))
+                return v.value;
+
+        return Integer.MIN_VALUE;
+    }
+
+    private static int[] findArray(String name) {
+        for(Array a : arrays)
+            if(a.name.equals(name))
+                return a.value;
+
+        return null;
+    }
+
+    public static float
     evaluate(String expr, ArrayList<Variable> vars, ArrayList<Array> arrays) {
-    	/** COMPLETE THIS METHOD **/
-    	// following line just a placeholder for compilation
+        Stack<String> operations = new Stack<String>();
+        Stack<Integer> data = new Stack<Integer>();
+
+        String ops = "+-*/";
+        expr = expr.replaceAll(" ", "");
+
+        while(expr.length() != 0) {
+            int i = 0;
+
+            while(expr.charAt(i) != '(' &&
+                    expr.charAt(i) != '[' &&
+                    !ops.contains(Character.toString(i)))
+                i++;
+
+            if(expr.charAt(i) == '[') {
+                data.push(findArray(expr.substring(i))[(int) evaluate(expr.substring(i + 1, expr.indexOf("]")), vars, arrays)]);
+                expr = expr.substring(expr.indexOf("]") + 1);
+            }
+
+            else if(ops.contains(Character.toString(expr.charAt(i)))) {
+                data.push(findValue(expr.substring(i - 1)));
+                operations.push(Character.toString(expr.charAt(i)));
+            }
+
+            else {
+                // code to evaluate normal cases
+            }
+
+        }
+
+        // evaluate stacks
+
     	return 0;
     }
 }
